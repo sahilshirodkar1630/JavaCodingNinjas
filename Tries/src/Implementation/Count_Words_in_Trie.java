20297bda486199a27958c1695eb9189bf104e3ff
sahilshirodkar1630 <sahilshirodkar1630@gmail.com>
2021-02-24 23:14:38 +0530

    Count Words in Trie

--- Tries/src/Implementation/Count_Words_in_Trie.java
+++ Tries/src/Implementation/Count_Words_in_Trie.java
@@ -2,7 +2,22 @@ package Implementation;
 
 public class Count_Words_in_Trie {
 
+//	You are given the Trie class with following functions -
+//	1. insertWord
+//	2. removeWord
+//	Now, you need to create one more function (named "countWords" ) 
+//	which returns the number of words currently present in Trie in O(1) time complexity
+	
+//Approach : We are using recursive approach for this  	
+//	1.First go to every TrieNode and check whether its not null 
+//	  i) if Yes move to Stepp 2;
+//	  ii) If No check the next TrieNode
+//	2.Check whether that node isTerminating 
+//	   i)if Yes Increment the count by 1;
+//	   ii) if No pass the childroot to recursion and increment the count by the recursion output; 
+//	3. Return the count;
 	TrieNode root;
+
 	private int countWords(TrieNode root) {
 		int count=0;
 		for(int i=0;i<26;i++) {
