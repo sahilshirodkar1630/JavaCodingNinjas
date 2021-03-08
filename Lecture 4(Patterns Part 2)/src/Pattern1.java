ca670e2813805b749edbb44973f208c33bcd4f1c
sahilshirodkar1630 <sahilshirodkar1630@gmail.com>
2021-03-08 23:52:00 +0530

    Pattern 1

--- Lecture 4(Patterns Part 2)/src/Pattern1.java
+++ Lecture 4(Patterns Part 2)/src/Pattern1.java
@@ -12,21 +12,47 @@ public class Pattern1 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n=sc.nextInt();
-		int i=1;
-		while(i<=n) {
-			int k=1;
-			while(k<=n-i) {
+		
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=0;j<=n-i;j++) {
 				System.out.print(" ");
-				k++;
 			}
-			k=1;
-			while(k<=i) {
-				System.out.print("*");
-				k++;
+			for(int j=0;j<i;j++) {
+				System.out.print("#");
 			}
-			i++;
 			System.out.println();
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		int i=1;
+//		while(i<=n) {
+//			int k=1;
+//			while(k<=n-i) {
+//				System.out.print(" ");
+//				k++;
+//			}
+//			k=1;
+//			while(k<=i) {
+//				System.out.print("*");
+//				k++;
+//			}
+//			i++;
+//			System.out.println();
+//		}
 
 	}
 
